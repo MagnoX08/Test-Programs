94707a3ac87d0e1d4bbf2ab61a136a5613f14145
MagnoX08 <kayrayer0@gmail.com>
2025-10-20 18:24:23 +0300

    Add files via upload
    C dilinde girilen sayıların arasındaki asalların bulunup toplanması için program

---
+++ Asal sayı toplama.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int main() {
+    
+    setlocale(LC_ALL, "Turkish"); 
+
+    int sayi1, sayi2;
+    int i, j;
+    int toplam = 0;
+    int asal_mi;
+    int temp;
+
+    
+    printf("Birinci sayýyý giriniz: ");
+    scanf("%d", &sayi1);
+
+    printf("Ýkinci sayýyý giriniz: ");
+    scanf("%d", &sayi2);
+
+    if (sayi1 > sayi2) 
+	{
+        temp = sayi1;
+        sayi1 = sayi2;
+        sayi2 = temp;
+    }
+
+    printf("\n%d ve %d arasýndaki asal sayýlar: ", sayi1, sayi2);
+
+    for (i = sayi1; i <= sayi2; i++) 
+	{
+
+        asal_mi = 1;
+
+        
+        if (i <= 1) 
+		{
+        asal_mi = 0;
+        
+        } 
+        
+        for (j = 2; j < i; j++) 
+		{
+            
+       
+        if (i % j == 0) {
+            asal_mi = 0;
+            break;
+            }
+        }
+
+        if (asal_mi == 1) 
+		{
+            printf("%d ", i);
+            toplam = toplam + i;
+        }
+    }
+    printf("\n\nBu aralýktaki asal sayýlarýn toplamý: %d\n", toplam);
+    getch();
+	}
